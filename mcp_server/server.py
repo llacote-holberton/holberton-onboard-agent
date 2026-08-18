6df3fda3ede3b0446e1e799390aa8ca257101084
Hugol4ka <hugolaca12300@gmail.com>
2026-08-18 16:08:49 +0200

    feat: add initial Docker setup and mcp server implementation

---
+++ mcp_server/server.py
@@ -0,0 +1,17 @@
+from mcp_instance import mcp
+import tools.tracker
+import tools.employee_db
+import tools.mailbox
+import tools.documents
+import tools.calendar
+import os
+
+MCP_PORT = int(os.getenv("HBN_MCP_PORT", "5003"))
+
+if __name__ == "__main__":
+    mcp.run(
+        transport="streamable-http",
+        host="0.0.0.0",
+        port=MCP_PORT,
+        show_banner=False,
+    )
